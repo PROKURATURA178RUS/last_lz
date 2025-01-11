4575d6cc0301a0ed1774a08ea4ec29d95fc58291
PROKURATURA178RUS <priora05tonirovka@tutamail.com>
2025-01-11 12:47:03 +0300

    camit

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import Archimed as arh # Программа выясняющая плавучесть объекта
 import pyramid as py # Программа считающая объем усеченной пирамиды
-
+import life_zone as life # Программа вычисляющая средний радиус обитаемой зоны
 
 def main():
-    user_choise = input('Введите 1 если arh\nВведите 2 если py\nВвод:')
+    user_choise = input('Введите 1 если arh\nВведите 2 если py\nВведите 3 если life\nВвод:')
     if user_choise.lower() == '1':
         arh.main()
     elif user_choise.lower() == '2':
         py.main()
+    elif user_choise.lower() == '3':
+        life.main()
     else:
         print('Не верно')
         main()
